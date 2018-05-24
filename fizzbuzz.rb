c47811c48ffe92ff70f3f30c7fe1d21cc92bc236
Changmin Lim <chang.lim92@gmail.com>
2018-05-24 14:58:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
-    "FizzBuzz"
+    return "FizzBuzz"
   elsif int % 5 == 0
-    "Buzz"
+    return "Buzz"
   else 
-    "Fizz"
+    return "Fizz"
   end
 end
